317697461cda73b801e65adeefc368d31993a92b
Rayan25166 <rayanjoomy772@gmail.com>
2025-07-16 10:13:36 +0530

    Add files via upload

---
+++ fiesta.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class fiesta {
+	public static void main(String[]args) {
+		Scanner n=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int i;
+		int number=n.nextInt();
+		for(i=0;i<number;i++)
+		{
+			System.out.println(i);
+			
+		}
+		n.close();
+				
+	}
+
+}
