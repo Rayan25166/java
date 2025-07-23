f967692cd22bdbd5f61a1a172967adaba56c9f8a
RAYAN <rayanjoomy772@gmail.com>
2025-07-23 09:25:01 +0530

    Add files via upload

---
+++ mustang.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class mustang {
+	public static void main (String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the mark:");
+		int n=sc.nextInt();
+		if(n>=90) {
+			System.out.println("The grade is A");}
+			else if(n>=75) {
+				System.out.println("The grade is B");}
+		
+				else if(n>=60) {
+					System.out.println("The grade is C");}
+				else if(n>=40) {
+					System.out.println("The grade is D");}
+				else {
+					System.out.println("The grade is fail");
+				}
+				}
+				}
+			
+			
+		
+	
+
+
